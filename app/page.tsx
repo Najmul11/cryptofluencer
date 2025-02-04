37eecd58366c8dab820ada2e229b241c7d11e6a6
Najmul <najmulbinnurul@gmail.com>
2025-02-05 05:49:38 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
 import AngledLines from "@/components/ui/AngledLines";
 
 const page = () => {
   return (
-    <div className="p-5 flex min-h-screen gap-5 max-w-[2400px] mx-auto">
-      <div className=" w-3/4 rounded-xl border border-neutral-200/80 p-5 relative overflow-hidden">
+    <div className=" flex min-h-screen gap-5 max-w-[2400px] mx-auto p-5">
+      {/* ######  left  */}
+      <div className=" w-3/4 rounded-xl  border  relative overflow-hidden flex-center border-neutral-200/80">
         <AngledLines />
-        <img src="logo.jpg" alt="" className="rounded-full size-12" />
+
+        <div className="place-items-center z-10 pt-20">
+          <h2 className="text-6xl text-center font-semibold">
+            Compho Airdrops
+          </h2>
+
+          <div className="relative w-[50%]  aspect-[16/9] z-10  rounded-2xl bg-black/20 backdrop-blur-sm border border-white/10 p-2">
+            {/* Image container */}
+            <div className="relative rounded-xl overflow-hidden aspect-video">
+              <img
+                src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b"
+                alt="Placeholder"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="w-1/4 rounded-xl border border-neutral-200/80"></div>
+
+      {/* #### right column */}
+      <div className="w-1/4 border rounded-xl  border-neutral-200/80"></div>
     </div>
   );
 };
