6c80c2ee1e5a548f6912b3159987c7e25bf3f8f7
Nur Mohammad Sadek Ali <sadekali1530@gmail.com>
2025-03-01 22:32:41 +0600

    make aridrops catagory routes

---
+++ app/airdrops/box/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const box = () => {
+    return (
+        <div>
+            <h1>this is box page .......</h1>
+        </div>
+    );
+};
+
+export default box;
---
+++ app/airdrops/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AirDrops = () => {
+    return (
+        <div>
+            <h1>this is aridrops </h1>
+        </div>
+    );
+};
+
+export default AirDrops;
