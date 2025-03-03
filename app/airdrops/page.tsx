1af491068700cf6dd012d5f87ecfd8fbd1012dc9
Nur Mohammad Sadek Ali <sadekali1530@gmail.com>
2025-03-03 22:50:31 +0600

    create layout

--- app/airdrops/page.tsx
+++ app/airdrops/page.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const AirDrops = () => {
     return (
-        <div className='border-red-500 max-w-screen-xl mx-auto h-screen'>
+        <div className='border-red-500 max-w-screen-xl mx-auto h-screen  '>
           <div className='flex w-full h-full '>
-            <div className='bg-slate-500 h-full w-1/5 rounded-l-lg'>
+            <div className='bg-slate-500 h-full w-1/5 rounded-l-lg md:w-1/4'>
                 <h1> dash board ...... </h1>
             </div>
-            <div className='bg-red-200 h-full w-4/5 rounded-r-lg'>
+            <div className='bg-red-200 h-full w-4/5 rounded-r-lg '>
                 <h1> contant ......</h1>
             </div>
           </div>
