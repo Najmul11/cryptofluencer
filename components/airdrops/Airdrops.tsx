3e05ac8bf4cd8eebac97624a779ea7bc30fd3c31
Najmul <najmulbinnurul@gmail.com>
2025-03-04 13:43:30 +0600

    cards

--- components/airdrops/Airdrops.tsx
+++ components/airdrops/Airdrops.tsx
@@ -1,7 +1,72 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import CardGrids from "../ui/CardGrids";
+import Link from "next/link";
 
 const Airdrops = () => {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-4 gap-8 mt-10">
+      {drops.map((drop) => (
+        <Link
+          href={"/"}
+          key={drop.title}
+          className="relative group border rounded-2xl border-neutral-200/60 p-4"
+        >
+          <CardGrids />
+
+          <div className="flex flex-col gap-2">
+            <img src={drop.logo} alt="" className="size-12 rounded-xl" />
+
+            <p className="font-semibold text-[16px]">{drop.title}</p>
+            <p className="text-[15px]">{drop.description}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Airdrops;
+
+const drops = [
+  {
+    title: "Phyt.fun ",
+    description: "Engage with free testnet, mint the free NFT ",
+    logo: "/assets/phyt.jpg",
+  },
+  {
+    title: "Retro Bridge",
+    description: "Engage with free testnet, mint the free NFT ",
+    logo: "/assets/retro.png",
+  },
+  {
+    title: "Uplink",
+    description: "Engage with free testnet, mint the free NFT ",
+    logo: "/assets/uplink.jpg",
+  },
+  {
+    title: "Phyt.fun ",
+    description: "Engage with free testnet, mint the free NFT ",
+    logo: "/assets/phyt.jpg",
+  },
+  {
+    title: "Retro Bridge",
+    description: "Engage with free testnet, mint the free NFT ",
+    logo: "/assets/retro.png",
+  },
+  {
+    title: "Uplink",
+    description: "Engage with free testnet, mint the free NFT ",
+    logo: "/assets/uplink.jpg",
+  },
+  {
+    title: "Retro Bridge",
+    description: "Engage with free testnet, mint the free NFT ",
+    logo: "/assets/retro.png",
+  },
+  {
+    title: "Uplink",
+    description: "Engage with free testnet, mint the free NFT ",
+    logo: "/assets/uplink.jpg",
+  },
+];
---
+++ components/ui/CardGrids.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+const CardGrids = () => {
+  return (
+    <div className="pointer-events-none opacity-">
+      <div className="absolute inset-0 rounded-2xl transition duration-300 [mask-image:linear-gradient(white,transparent)] group-hover:opacity-50">
+        <svg
+          aria-hidden="true"
+          className="absolute inset-x-0 inset-y-[-30%] h-[160%] w-full skew-y-[-18deg] fill-black/[0.02] stroke-black/5"
+        >
+          <defs>
+            <pattern
+              id="pattern1"
+              width="72"
+              height="56"
+              patternUnits="userSpaceOnUse"
+              x="50%"
+              y="16"
+            >
+              <path d="M.5 56V.5H72" fill="none"></path>
+            </pattern>
+          </defs>
+          <rect
+            width="100%"
+            height="100%"
+            strokeWidth="0"
+            fill="url(#pattern1)"
+          ></rect>
+          <svg x="50%" y="16" className="overflow-visible">
+            <rect strokeWidth="0" width="73" height="57" x="0" y="56"></rect>
+            <rect strokeWidth="0" width="73" height="57" x="72" y="168"></rect>
+          </svg>
+        </svg>
+      </div>
+      <div
+        className="absolute inset-0 rounded-2xl bg-gradient-to-r from-[#D7EDEA] to-brand opacity-0 transition duration-300 group-hover:opacity-100"
+        style={{
+          maskImage:
+            "radial-gradient(300px at 409.5px 128px, white, transparent)",
+        }}
+      ></div>
+      <div
+        className="absolute inset-0 rounded-2xl opacity-0 mix-blend-overlay transition duration-300 group-hover:opacity-100"
+        style={{
+          maskImage:
+            "radial-gradient(300px at 409.5px 128px, white, transparent)",
+        }}
+      >
+        <svg
+          aria-hidden="true"
+          className="absolute inset-x-0 inset-y-[-30%] h-[160%] w-full skew-y-[-18deg] fill-black/50 stroke-black/70"
+        >
+          <defs>
+            <pattern
+              id="pattern2"
+              width="72"
+              height="56"
+              patternUnits="userSpaceOnUse"
+              x="50%"
+              y="16"
+            >
+              <path d="M.5 56V.5H72" fill="none"></path>
+            </pattern>
+          </defs>
+          <rect
+            width="100%"
+            height="100%"
+            strokeWidth="0"
+            fill="url(#pattern2)"
+          ></rect>
+          <svg x="50%" y="16" className="overflow-visible">
+            <rect strokeWidth="0" width="73" height="57" x="0" y="56"></rect>
+            <rect strokeWidth="0" width="73" height="57" x="72" y="168"></rect>
+          </svg>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default CardGrids;
