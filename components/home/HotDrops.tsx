c9e57cdb6ec68674a0de513fb8212b004ed42897
Najmul <najmulbinnurul@gmail.com>
2025-03-10 08:25:58 +0600

    Update HotDrops.tsx

--- components/home/HotDrops.tsx
+++ components/home/HotDrops.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 const HotDrops = () => {
   return (
     <div className="mt-5 grid grid-cols-3 gap-5">
-      {links.map((link) => (
+      {links.map((link, index) => (
         <Link
           key={link.label}
           href={link.path}
           className="py-4 border-2 border-brand hover:text-white group duration-200 rounded-lg flex justify-center items-center text-xl font-semibold relative overflow-hidden"
         >
-          <span className="text-2xl">🔥</span> {link.label}
+          {index !== 2 && <span className="text-2xl">🔥</span>} {link.label}
           {/* Background transition effect */}
-          <span className="absolute h-[300px] -translate-x-2 group-hover:translate-x-0 w-full bg-brand scale-x-[20%] rotate-6 group-hover:rotate-0 group-hover:scale-x-100 transition-transform duration-300 origin-left -z-10 block"></span>
+          <span className="absolute h-[300px] -translate-x-2 group-hover:translate-x-0 w-full bg-brand scale-x-[10%] rotate-6 group-hover:rotate-0 group-hover:scale-x-100 transition-transform duration-300 origin-left -z-10 block"></span>
         </Link>
       ))}
     </div>
