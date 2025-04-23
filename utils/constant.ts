2b2faf79f03ce8ded35980ef3fa709bf9694d595
Najmul <najmulbinnurul@gmail.com>
2025-04-23 21:03:23 +0600

    Update constant.ts

--- utils/constant.ts
+++ utils/constant.ts
@@ -2,15 +2,15 @@
 const ENV_MODE: string = process.env.MODE || "production";
 
 const getEnvUrls = () => {
-  let apiUrl;
-  let baseUrl;
+  let apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  let baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
   if (ENV_MODE === "development") {
     apiUrl = "http://localhost:5001/api/v1";
     baseUrl = "http://localhost:5001/v1";
   } else {
-    apiUrl = "http://localhost:5001/api/v1";
-    baseUrl = "http://localhost:5001/";
+    apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   }
 
   return { apiUrl, baseUrl, ENV_MODE };
