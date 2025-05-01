5bb41f9b6af20d3c54bd251603be49d92c7155ea
Najmul <najmulbinnurul@gmail.com>
2025-05-01 07:32:44 +0600

    banner fixed

--- components/home/BannerAds.tsx
+++ components/home/BannerAds.tsx
@@ -2,78 +2,57 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import { useRouter } from "next/navigation";
-import Slider from "react-slick";
-import { useRef, useState } from "react";
 import { useGetAllBannersQuery } from "@/redux/api/banner";
-import Link from "next/link";
 import BannerSkeleton from "../skeleton/BannerSkeleton";
+
+// Swiper imports
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import Link from "next/link";
 
 const BannerAds = () => {
   const router = useRouter();
-  const [isSwiping, setIsSwiping] = useState(false);
-  const touchStartX = useRef(0);
-  const touchEndX = useRef(0);
-
   const { data, isLoading } = useGetAllBannersQuery("");
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    beforeChange: () => setIsSwiping(true),
-    afterChange: () => setTimeout(() => setIsSwiping(false), 100),
-  };
-
-  const handleTouchStart = (e: any) => {
-    touchStartX.current = e.touches[0].clientX;
-  };
-
-  const handleTouchEnd = (e: any) => {
-    touchEndX.current = e.changedTouches[0].clientX;
-    if (Math.abs(touchStartX.current - touchEndX.current) > 10) {
-      setIsSwiping(true);
-    }
-    setTimeout(() => setIsSwiping(false), 300);
-  };
-
-  const handleClick = () => {
-    if (!isSwiping) {
-      router.push("/retro-bridge");
-    }
-  };
-
   return (
-    <div className="">
+    <div className="w-full">
       {isLoading ? (
         <BannerSkeleton />
       ) : (
-        <>
-          {" "}
-          {data?.data?.length > 0 && (
-            <Slider {...settings}>
-              {data?.data.map((banner: any) => (
-                <Link
-                  href={banner?.project?.slug}
-                  key={banner.id}
-                  className="block cursor-pointer  px-2  "
-                  onClick={handleClick}
-                  onTouchStart={handleTouchStart}
-                  onTouchEnd={handleTouchEnd}
-                >
-                  <img
-                    src={banner?.imageUrl}
-                    alt="banner"
-                    className="w-full aspect-[16/9] rounded-lg "
-                  />
-                </Link>
-              ))}
-            </Slider>
-          )}
-        </>
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={16}
+          autoplay={{ delay: 4000 }}
+          loop={true}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+            },
+            540: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 3,
+            },
+            1024: {
+              slidesPerView: 4,
+            },
+          }}
+        >
+          {data?.data?.map((banner: any) => (
+            <SwiperSlide key={banner.id}>
+              <Link href={banner?.project?.slug} className="">
+                <img
+                  src={banner.imageUrl}
+                  alt={banner.title || "Banner Ad"}
+                  className="w-full h-auto object-cover rounded-md cursor-pointer aspect-[16/9]"
+                  onClick={() => router.push(banner.redirectUrl)}
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       )}
     </div>
   );
