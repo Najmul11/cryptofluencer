0e36233eed40c3bab924c09077020074c39521c7
Najmul <najmulbinnurul@gmail.com>
2025-05-01 07:35:52 +0600

    Update BannerSkeleton.tsx

--- components/skeleton/BannerSkeleton.tsx
+++ components/skeleton/BannerSkeleton.tsx
@@ -2,9 +2,9 @@ import Skeleton from "react-loading-skeleton";
 
 const BannerSkeleton = () => {
   return (
-    <div className="grid grid-cols-4 gap-5  ">
-      {Array.from({ length: 4 }).map((_, i) => (
-        <Skeleton key={i} className="h-[170px]" />
+    <div className="  ">
+      {Array.from({ length: 1 }).map((_, i) => (
+        <Skeleton key={i} className="h-[170px] w-full" />
       ))}
     </div>
   );
